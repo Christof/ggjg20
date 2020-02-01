8baeeeebaeed006ce210a5841cab87627dc1afc2
Tobias <pingutz@tug-swl-225-104.tugraz.at>
2020-02-01 14:14:49 +0100

    abstracted gamepad away

--- src/input.js
+++ src/input.js
@@ -2,21 +2,24 @@ const Input = {
   gamepad_connected: false,
   gamepad_index: null,
   current: {
-    key: null
+    keys: {}
   },
   getGamepadJoystick() {
-    if (this.gamepad_index != null) {
+    if (!this.gamepad_connected) {
       let gamepad = navigator.getGamepads()[this.gamepad_index];
       return [gamepad.axes[0], gamepad.axes[1]];
     }
   },
   isDown(key) {
-    if (key == this.current.key) {
+    if (this.current.keys[key]) {
       return true;
+    }
+    else {
+      return false;
     }
   },
   hasGamepadMovementAboveThreshold() {
-    if (this.gamepad_index == null) return false;
+    if (!this.gamepad_connected) return false;
 
     const [horizontal, vertical] = this.getGamepadJoystick();
     const movementThreshold = 0.1;
@@ -24,25 +27,65 @@ const Input = {
       Math.abs(horizontal) > movementThreshold &&
       Math.abs(vertical) > movementThreshold
     );
+  },
+  moveLeft() {
+    if (this.hasGamepadMovementAboveThreshold()) {
+      if (this.getGamepadJoystick()[0] < 0) {
+        return true;
+      }
+    }
+    if (this.isDown("ArrowLeft")) {
+      return true;
+    }
+  },
+  moveRight() {
+    if (this.hasGamepadMovementAboveThreshold()) {
+      if (this.getGamepadJoystick()[0] > 0) {
+        gamepad_movement = true;
+      }
+    }
+    if (this.isDown("ArrowRight")) {
+      return true;
+    }
+  },
+  moveUp() {
+    if (this.hasGamepadMovementAboveThreshold()) {
+      if (this.getGamepadJoystick()[1] < 0) {
+        return true;
+      }
+    }
+    if (this.isDown("ArrowUp")) {
+      return true;
+    }
+  },
+  moveDown() {
+    if (this.hasGamepadMovementAboveThreshold()) {
+      if (this.getGamepadJoystick()[0] > 0) {
+        return true;
+      }
+    }
+    if (this.isDown("ArrowDown")) {
+      return true;
+    }
   }
 };
 
-window.addEventListener('gamepadconnected', ev => {
+window.addEventListener("gamepadconnected", ev => {
   Input.gamepad_index = ev.gamepad.index;
   Input.gamepad_connected = true;
 });
 
-window.addEventListener('gamepaddisconnected', ev => {
+window.addEventListener("gamepaddisconnected", ev => {
   Input.gamepad_index = null;
   Input.gamepad_connected = false;
 });
 
-window.onkeydown = ev => {
-  Input.current.key = ev.key;
+window.onkeydown = (ev) => {
+  Input.current.keys[ev.key] = true;
 };
 
-window.onkeyup = () => {
-  Input.current.key = null;
+window.onkeyup = (ev) => {
+  Input.current.keys[ev.key] = false;
 };
 
 module.exports = Input;
