59737537c63b07645826ea98b6f8ed1c0012f633
Tobias <pingutz@tug-swl-225-104.tugraz.at>
2020-02-02 12:39:37 +0100

    healthbar

--- src/bar.ts
+++ src/bar.ts
@@ -1,17 +1,30 @@
-import { Sprite, Texture, Container } from 'pixi.js';
+import { Sprite, Texture, Container, Graphics, graphicsUtils } from 'pixi.js';
 
 export class Bar {
-    percentage = 0;
+    percentage = 1;
     container: Container
-    layer0: Sprite;
-    layer1: Sprite;
-    layer2: Sprite;
+    private mask: Graphics;
+    private layer0: Sprite;
+    private layer1: Sprite;
+    private layer2: Sprite;
 
     constructor(layer0: Texture, layer1: Texture, layer2: Texture) {
         this.container = new Container();
+        this.mask = new Graphics();
+        this.mask.beginFill(0xF);
+        this.mask.drawRect(0, 0, 64, 128);
+        this.mask.endFill();
         this.layer0 = new Sprite(layer0);
         this.layer1 = new Sprite(layer1);
+        this.layer1.mask = this.mask;
         this.layer2 = new Sprite(layer2);
         this.container.addChild(this.layer0, this.layer1, this.layer2);
+    }
+
+    public set(percentage: number) {
+        this.percentage = percentage;
+        this.mask.clear();
+        this.mask.beginFill(0xF);
+        this.mask.drawRect(0, 0, 64, 128*percentage);
     }
 }
